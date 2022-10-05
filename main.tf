3ca136f8fc832337b20dee2c0b76d511195b2feb
Kyra Rodriguez <kyrarodriguez89@hotmail.com>
2022-10-05 03:11:34 +0000

    updating and adding port22

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/home/kyr4142/cis91-001-27d6623fc037.json")
+
+  project = "cis91-001"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+output "ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.network_ip
+}
