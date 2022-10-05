07e0993f5edcbfcc55e74d4321fe6c49d451efd7
Kyra Rodriguez <kyrarodriguez89@hotmail.com>
2022-10-05 03:15:38 +0000

    trying to update port 22 #2 try

--- base/main.tf
+++ base/main.tf
@@ -1,20 +1,52 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "3.5.0"
     }
   }
 }
 
 provider "google" {
-  credentials = file("even-flight-363618-e659b3919ef5.json")
+  credentials = file("/home/kyr4142/rodriguez-364418-f0b1d1a65e7c.json")
 
-  project = "even-flight-363618"
+  project = "rodriguez-364418"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_firewall" "default-firewall" {
+  name = "default-firewall"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+output "ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.network_ip
 }
--- bin/yaml-check.py
+++ bin/yaml-check.py
@@ -4,3 +4,4 @@ import sys
 
 data = yaml.load(sys.stdin.read(), Loader=yaml.Loader)
 pprint.pprint(data, compact=False, sort_dicts=False)
+
