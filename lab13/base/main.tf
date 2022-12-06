a41fd5fd9009be36ba3beb7c78ac3f130da56f14
Kyra Rodriguez <kyrarodriguez89@hotmail.com>
2022-12-06 06:16:38 +0000

    network update to fix data crash

--- lab13/base/main.tf
+++ lab13/base/main.tf
@@ -33,7 +33,7 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "terraform-network1"
 }
 
 resource "google_compute_instance" "webservers" {
