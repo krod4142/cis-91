db2f5bcad4a0466c642b30e07e081ef35f0b9f1f
Kyra Rodriguez <kyrarodriguez89@hotmail.com>
2022-12-16 06:02:43 +0000

    global address added to main

--- base/main.tf
+++ base/main.tf
@@ -117,6 +117,10 @@ resource "google_compute_target_http_proxy" "default" {
   url_map  = google_compute_url_map.default.id
 }
 
+resource "google_compute_global_address" "default" {
+  name = "external-address"
+}
+
 output "external-ip" {
   value = google_compute_instance.webservers[*].network_interface[0].access_config[0].nat_ip
 }
