422ab6f603c88078733cb45dd7fc676599294ea0
Kyra Rodriguez <kyrarodriguez89@hotmail.com>
2022-12-16 04:10:39 +0000

    replace out external ip

--- base/main.tf
+++ base/main.tf
@@ -70,5 +70,5 @@ resource "google_compute_firewall" "default-firewall" {
 }
 
 output "external-ip" {
-  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.webservers[*].network_interface[0].access_config[0].nat_ip
 }
