3ccfef9d9a878be468c762ffd5de7f0c72938a1c
Kyra Rodriguez <kyrarodriguez89@hotmail.com>
2022-12-16 04:08:36 +0000

    update main line 40 from load balance

--- base/main.tf
+++ base/main.tf
@@ -37,8 +37,9 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = "true"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "cis91"
+resource "google_compute_instance" "webservers" {
+  count        = 2
+  name         = "web${count.index}"
   machine_type = "e2-micro"
 
   boot_disk {
@@ -51,6 +52,10 @@ resource "google_compute_instance" "vm_instance" {
     network = google_compute_network.vpc_network.name
     access_config {
     }
+  }
+
+  labels = {
+    role: "web"
   }
 }
 
